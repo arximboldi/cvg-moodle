950d65891897bf1744742fb4c768455cbe0122d3
Juan Pedro Bolivar Puente <raskolnikov@es.gnu.org>
2011-01-02 21:42:08 +0100

    Added the fileresponse question type.

---
+++ question/type/fileresponse/lang/de_utf8/qtype_fileresponse.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * The language strings for the fileresponse question type.
+ *    
+ * @copyright &copy; 2007 Adriane Boyd
+ * @author adrianeboyd@gmail.com
+ * @license http://www.gnu.org/copyleft/gpl.html GNU Public License
+ * @package question_fileresponse
+ *//** */
+
+$string['addingfileresponse'] = 'Dateibeschreibung hinzufügen';
+$string['editingfileresponse'] = 'Dateibeschreibung bearbeiten';
+$string['fileresponse'] = 'Frage mit Dateiupload';
+$string['uploaderror'] = 'Es wurde keine Datei übertragen - mögliche Gründe: Sie haben keine Datei zum Upload angegeben, die Datei konnte nicht gefunden werden oder die Datei war zu groß.';
+$string['uploadedfile'] = 'Derzeit übertragene Datei';
+$string['uploadnew'] = 'Ersetze die aktuell übertragene Datei mit einer aktuelleren Datei.';
+$string['addessay'] = 'Feld für Dateibeschreibung hinzufügen';
+
+?>
